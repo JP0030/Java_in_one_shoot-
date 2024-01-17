da957e246b781d1a7157497621b1e3f3f4dd5cc2
JP0030 <thekeen003@gmail.com>
2024-01-17 21:49:44 +0530

    'Hello world!'the first code as a devloper you must have to write , because it's your firststape in the world of new language as a beginner😊

---
+++ Hello_Java.java
@@ -0,0 +1,11 @@
+public class Hello_Java {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("How are you");
+        System.out.println(" , are you ok");
+        System.out.print("Yaa i'm all ok");
+
+
+    }
+
+}
