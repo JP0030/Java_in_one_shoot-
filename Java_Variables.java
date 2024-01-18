c4239ec430c2eb8a65e10eb589af0b788ec87a17
JP0030 <thekeen003@gmail.com>
2024-01-18 18:54:37 +0530

    added new comment at 68:-row

--- Java_Variables.java
+++ Java_Variables.java
@@ -18,31 +18,36 @@ public class Java_Variables {
         // basic structure tu define variable
         // type variableName = value;
         // type represent the variable's type like 'String' or 'int'
-        // variableName reprecent the name of variable 
+        // variableName reprecent the name of variable
         // vlaue is represent the value of variable
-        // and the ' = ' assignment operator is assign the valu to the vavriable acording to it's type
+        // and the ' = ' assignment operator is assign the valu to the vavriable
+        // acording to it's type
 
         // now Create a variable called num of type int and assign it the value 18:
         int num = 18;
-        System.out.println(" The value of int num is :- " + num );
-        
-        //or 
+        System.out.println(" The value of int num is :- " + num);
 
-        /*int num ;
-        num = 18 ;
-        System.out.println(" The value of int num is :- " + num );
-        */  // this is the another way to define the variable
+        // or
 
-        // as par above we also overwrite the value to. by variable 'number' with value of 21 to overwrite 30 
+        /*
+         * int num ;
+         * num = 18 ;
+         * System.out.println(" The value of int num is :- " + num );
+         */ // this is the another way to define the variable
+
+        // as par above we also overwrite the value to. by variable 'number' with value
+        // of 21 to overwrite 30
         int number = 21;
         number = 30;
         System.out.println(" overwrited value of int number :- " + number);
 
-        // there are another method with you can prevent the value from overwrit... as 'final' key word
+        // there are another method with you can prevent the value from overwrit... as
+        // 'final' key word
         final int numBer = 30;
         System.out.println(" you can note overwrited value of int numBer :- " + numBer);
 
-        // now Create a variable called myName of type String and assign it the value "Nish":
+        // now Create a variable called myName of type String and assign it the value
+        // "Nish":
         String myName = "Nish";
         System.out.println(" The value of String myName is :-  " + myName);
 
@@ -54,16 +59,17 @@ public class Java_Variables {
         boolean myBool = true;
         String myText = "Hello";
 
-        System.out.println( + myNum +" , " + myFloatNum + " , "+ myBool+ " , " + myLetter + " , " + myText);
+        System.out.println(+myNum + " , " + myFloatNum + " , " + myBool + " , " + myLetter + " , " + myText);
 
         // you can also declair your variables like this
 
-        int a = 10 , b = 12 , c = 18 , d = 30 ;
+        int a = 10, b = 12, c = 18, d = 30;
 
-        int x = a + b + c + d ;
+        // You can also use the + character to add a variable to another variable:
+        int x = a + b + c + d;
 
         System.out.println(" The som of a + b + c + d = " + x);
 
     }
-    
+
 }
